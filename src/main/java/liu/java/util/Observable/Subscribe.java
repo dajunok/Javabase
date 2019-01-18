e34298e3d4c6d3e7b2675dc2a3c51e29bad77cd8
liulijun <1937249556@qq.com>
2019-01-19 00:15:26 +0800

    liu.java.util

--- src/main/java/liu/java/util/Observable/Subscribe.java
+++ src/main/java/liu/java/util/Observable/Subscribe.java
@@ -13,7 +13,7 @@ import java.util.Observer;
 public class Subscribe implements Observer{
 	
 	public Subscribe(Observable o) {
-		o.addObserver(this);   //将该观察者放入待通知观察者里
+		o.addObserver(this);   //将该观察者放入待通知者里
 	}
 
 	@Override
