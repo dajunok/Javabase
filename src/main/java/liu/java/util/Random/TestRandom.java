9a90b445de4894a3399bc0dc20d26db0f813b9ca
liulijun <1937249556@qq.com>
2019-01-22 18:12:35 +0800

    提交：liu.java.util

---
+++ src/main/java/liu/java/util/Random/TestRandom.java
@@ -0,0 +1,5 @@
+package liu.java.util.Random;
+
+public class TestRandom {
+
+}
---
+++ src/main/java/liu/java/util/ResourceBundle/ListResourceBundle/TestListResourceBundle.java
@@ -0,0 +1,5 @@
+package liu.java.util.ResourceBundle.ListResourceBundle;
+
+public class TestListResourceBundle {
+
+}
