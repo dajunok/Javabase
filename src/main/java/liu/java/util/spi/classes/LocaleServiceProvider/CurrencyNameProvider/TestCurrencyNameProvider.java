ba696ff867c8649c50eaca92487860d6d97c27cd
liulijun <1937249556@qq.com>
2019-03-23 20:21:55 +0800

    提交：liu.java.util.spi

---
+++ src/main/java/liu/java/util/spi/classes/LocaleServiceProvider/CurrencyNameProvider/TestCurrencyNameProvider.java
@@ -0,0 +1,5 @@
+package liu.java.util.spi.classes.LocaleServiceProvider.CurrencyNameProvider;
+
+public class TestCurrencyNameProvider {
+
+}
---
+++ src/main/java/liu/java/util/spi/classes/LocaleServiceProvider/LocaleNameProvider/TestLocaleNameProvider.java
@@ -0,0 +1,5 @@
+package liu.java.util.spi.classes.LocaleServiceProvider.LocaleNameProvider;
+
+public class TestLocaleNameProvider {
+
+}
