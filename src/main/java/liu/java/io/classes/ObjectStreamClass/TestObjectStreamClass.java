b782a7869d969a2b86be7778e5479f43fb61f699
liulijun <1937249556@qq.com>
2019-04-11 18:00:30 +0800

    提交：liu.java.io

---
+++ src/main/java/liu/java/io/classes/ObjectStreamClass/TestObjectStreamClass.java
@@ -0,0 +1,5 @@
+package liu.java.io.classes.ObjectStreamClass;
+
+public class TestObjectStreamClass {
+
+}
---
+++ src/main/java/liu/java/io/classes/ObjectStreamField/TestObjectStreamField.java
@@ -0,0 +1,5 @@
+package liu.java.io.classes.ObjectStreamField;
+
+public class TestObjectStreamField {
+
+}
